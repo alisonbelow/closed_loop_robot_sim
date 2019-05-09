f8bb787a9648e04d20f3d376ea14a83376a97148
alisonbelow <alisonbelow@gmail.com>
2019-05-08 23:24:05 -0400

    Fix permissions problem in for Docker in Virtualbox

--- docker.sh
+++ docker.sh
@@ -7,23 +7,22 @@ TAG="0.1"
 # Command to install docker
 if [ "$1" == "install" ]
 then
-    # sudo apt-get remove docker docker-engine docker.io
-    # sudo apt-get update
-    # sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
-    # curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    # sudo apt-key fingerprint 0EBFCD88
-    # sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    # sudo apt-get update
+    sudo apt-get remove docker docker-engine docker.io
+    sudo apt-get update
+    sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
     sudo apt-get install docker-ce
-    sudo docker run hello-world
 
     # Add your user to docker group to use with non-sudo access
-    # sudo groupadd docker
-    # sudo usermod -aG docker $USER       
-    
+    sudo groupadd docker
+    sudo usermod -aG docker $USER       
+    sudo chown ${USER} /var/run/docker.sock
+
     echo "*****************************"
-    echo "Please log in and out of your user account to use docker as a non-super user"
-    echo "Then you can proceed with testing your docker install with the command:"
+    echo "Please proceed with testing your docker install with the command:"
     echo "docker run hello-world"
     echo "*****************************"
     exit
